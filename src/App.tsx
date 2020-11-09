26e7f0c0f7ffe0afacf1f575971ec60b50ec846f
jaykaron <jaykaron@gmail.com>
2020-11-09 18:30:24 -0500

    Add app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import style from './App.module.css';
+import Button from './components/Button';
+import Card from './components/Card';
+import { ALL_BARA, exampleToString } from './data/trop';
 
 function App() {
+  const [index, setIndex] = useState<number>(0);
+
+  const ex = ALL_BARA[index];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={style.main}>
+      <h1 className={style.title}>Trop Cards</h1>
+      <Card key={ex.name} name={ex.name} hebrewText={exampleToString(ex)} />
+      <Button onClick={() => setIndex(randomIndex())}>
+        Random
+      </Button>
     </div>
   );
 }
 
 export default App;
+
+
+function randomIndex(): number {
+  return Math.floor(Math.random() * ALL_BARA.length);
+}
+
+
