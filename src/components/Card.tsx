0f5efd8832888c07122830a806bca56e310f7fc3
jaykaron <jaykaron@gmail.com>
2020-11-09 18:30:20 -0500

    Add card

---
+++ src/components/Card.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import style from './Card.module.css';
+
+interface CardProps {
+  name: string;
+  hebrewText: string;
+}
+
+const Card : React.FC<CardProps> = ({ name, hebrewText }) => (
+  <div className={style.container}>
+    <div className={style.main}>
+      <h2>{name}</h2>
+      <div className={style.hebrew_container}>
+        <span className={style.hebrew_text}>{hebrewText}</span>
+      </div>
+    </div>
+  </div>
+);
+
+export default Card;
