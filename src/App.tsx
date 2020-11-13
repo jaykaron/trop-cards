bd309bdba4be9a4c08936cc8fcdcc0fdda48a2d5
jaykaron <jaykaron@gmail.com>
2020-11-12 23:22:41 -0500

    Standard trop

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,31 @@ import React, { useState } from 'react';
 import style from './App.module.css';
 import Button from './components/Button';
 import Card from './components/Card';
-import { ALL_BARA, exampleToString } from './data/trop';
+import { randInt } from './data/utils';
+import { STANDARD } from './data/sets/archetypes';
 
 function App() {
   const [index, setIndex] = useState<number>(0);
 
-  const ex = ALL_BARA[index];
+  const list = STANDARD;
+  const ex = list[index];
+
+  const next = () => setIndex(i => (i + 1) % list.length);
+  const random = () => setIndex(randInt(list.length));
 
   return (
     <div className={style.main}>
       <h1 className={style.title}>Trop Cards</h1>
-      <Card key={ex.name} name={ex.name} hebrewText={exampleToString(ex)} />
-      <Button onClick={() => setIndex(randomIndex())}>
-        Random
-      </Button>
+      <Card key={ex.name} name={ex.name} hebrewText={ex.text} />
+
+      <div className={style.buttonContainer}>
+      <Button onClick={next}>Next</Button>
+      <Button onClick={random}>Random</Button>
+      </div>
     </div>
   );
 }
 
 export default App;
-
-
-function randomIndex(): number {
-  return Math.floor(Math.random() * ALL_BARA.length);
-}
 
 
