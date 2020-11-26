41f696d228109fc7eb079c06df89798c7a477ad2
jaykaron <jaykaron@gmail.com>
2020-11-25 22:02:01 -0500

    Don't export individual names

--- src/data/tropSymbols.ts
+++ src/data/tropSymbols.ts
@@ -1,33 +1,38 @@
 import * as NAMES from "./tropNames";
 
-export const ETNAHTA = String.fromCharCode(1425);
-export const SEGOL = String.fromCharCode(1426);
-export const SHALSHELET = String.fromCharCode(1427);
-export const ZAQEF_QATAN = String.fromCharCode(1428);
-export const ZAQEF_GADOL = String.fromCharCode(1429);
-export const TIPEHA = String.fromCharCode(1430);
-export const REVIA = String.fromCharCode(1431);
-export const ZARQA = String.fromCharCode(1432);
-export const PASHTA = String.fromCharCode(1433);
-export const YETIV = String.fromCharCode(1434);
-export const TEVIR = String.fromCharCode(1435);
-export const GERESH = String.fromCharCode(1436);
+const ETNAHTA = String.fromCharCode(1425);
+const SEGOL = String.fromCharCode(1426);
+const SHALSHELET = String.fromCharCode(1427);
+const ZAQEF_QATAN = String.fromCharCode(1428);
+const ZAQEF_GADOL = String.fromCharCode(1429);
+const TIPEHA = String.fromCharCode(1430);
+const REVIA = String.fromCharCode(1431);
+const ZARQA = String.fromCharCode(1432);
+const PASHTA = String.fromCharCode(1433);
+const YETIV = String.fromCharCode(1434);
+const TEVIR = String.fromCharCode(1435);
+const GERESH = String.fromCharCode(1436);
 
-export const GERSHAYIM = String.fromCharCode(1438);
-export const QARNEY_PARA = String.fromCharCode(1439);
-export const TELISHA_GEDOLA = String.fromCharCode(1440);
-export const PAZER = String.fromCharCode(1441);
-export const ATNAH_HAFUKH = String.fromCharCode(1442);
-export const MUNAH = String.fromCharCode(1443);
-export const MAHAPAKH = String.fromCharCode(1444);
-export const MERKHA = String.fromCharCode(1445);
-export const MERKHA_KEFULA = String.fromCharCode(1446);
-export const DARGA = String.fromCharCode(1447);
-export const QADMA = String.fromCharCode(1448);
-export const TELISHA_KETANA = String.fromCharCode(1449);
-export const YERAH_BEN_YOMO = String.fromCharCode(1450);
+const GERSHAYIM = String.fromCharCode(1438);
+const QARNEY_PARA = String.fromCharCode(1439);
+const TELISHA_GEDOLA = String.fromCharCode(1440);
+const PAZER = String.fromCharCode(1441);
+const ATNAH_HAFUKH = String.fromCharCode(1442);
+const MUNAH = String.fromCharCode(1443);
+const MAHAPAKH = String.fromCharCode(1444);
+const MERKHA = String.fromCharCode(1445);
+const MERKHA_KEFULA = String.fromCharCode(1446);
+const DARGA = String.fromCharCode(1447);
+const QADMA = String.fromCharCode(1448);
+const TELISHA_KETANA = String.fromCharCode(1449);
+const YERAH_BEN_YOMO = String.fromCharCode(1450);
 
-export const STANDARD_SYMBOLS = [
+export interface Symbol {
+  name: NAMES.ALL_NAMES;
+  char: string;
+}
+
+export const STANDARD_SYMBOLS : Symbol[] = [
   { name: NAMES.ETNAHTA, char: ETNAHTA},
   { name: NAMES.SEGOL, char: SEGOL},
   { name: NAMES.ZAQEF_QATAN, char: ZAQEF_QATAN},
@@ -50,7 +55,7 @@ export const STANDARD_SYMBOLS = [
   { name: NAMES.TELISHA_KETANA, char: TELISHA_KETANA},
 ];
 
-export const RARE_SYMBOLS = [
+export const RARE_SYMBOLS : Symbol[] = [
   { name: NAMES.SHALSHELET, char: SHALSHELET},
   { name: NAMES.QARNEY_PARA, char: QARNEY_PARA},
   { name: NAMES.ATNAH_HAFUKH, char: ATNAH_HAFUKH},
@@ -58,4 +63,4 @@ export const RARE_SYMBOLS = [
   { name: NAMES.YERAH_BEN_YOMO, char: YERAH_BEN_YOMO}
 ];
 
-export const ALL_SYMBOLS = [...STANDARD_SYMBOLS, ...RARE_SYMBOLS];
+export const ALL_SYMBOLS : Symbol[] = [...STANDARD_SYMBOLS, ...RARE_SYMBOLS];
